0d4f3a10ed770568e5bb7668ba7f0d8005debd74
akulah91 <akulah91@gmail.com>
2019-01-08 15:08:54 +0800

    Updates

---
+++ precache-manifest.0e957752e77a49c4c7086e92e7c864f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rhb-assessment/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "74cd46553a948ad3a950",
+    "url": "/rhb-assessment/static/js/runtime~main.74cd4655.js"
+  },
+  {
+    "revision": "d2fb8c0e3ceca982d659",
+    "url": "/rhb-assessment/static/js/main.d2fb8c0e.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/rhb-assessment/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d2fb8c0e3ceca982d659",
+    "url": "/rhb-assessment/static/css/main.4fac0a15.chunk.css"
+  },
+  {
+    "revision": "62db2f47fb9cc0ee663342deb45f2f82",
+    "url": "/rhb-assessment/index.html"
+  }
+];
